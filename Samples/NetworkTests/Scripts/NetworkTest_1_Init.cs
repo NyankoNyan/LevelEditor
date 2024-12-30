c4ab696aaec9c2c70534df51796ac38c017abd69
Nyanko Nyan <gverycrazy@gmail.com>
2024-12-30 15:08:28 +0300

    +

---
+++ Samples/NetworkTests/Scripts/NetworkTest_1_Init.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Level.Samples
+{
+    public class NetworkTest_1_Init : MonoBehaviour
+    {
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+    }
+}
